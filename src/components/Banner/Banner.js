d3cea1e5f4281e35e3c283d2744d7416724c1384
namacocnut <namacoconut@gmail.com>
2022-05-10 13:59:45 +0300

    adding search component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import '../Banner/Banner.css'
+import { useState } from "react";
 import Button from '@mui/material/Button';
-// import Banner from '../../images/banner.png'
-
+import Search from '../Search/Search'
 
 function Banner() {
+    const [showSearch, setShowSearch] = useState (false);
     return (
         <div className='banner'>
+        <div className='banner--search'>
+        {showSearch && <Search/>}
+            <Button 
+            onClick ={()=> setShowSearch(!showSearch)}
+            variant="outlined"
+            className='banner--explore--btn'>Search Dates</Button>
+            </div>
             <div className='banner--info'>
             <h1> Get Out and Stretch your imagination</h1>
             <h3> plan a different kind of getway </h3>
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import '../Search/Search.css'
+import {DateRangePicker} from 'react-date-range'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { useState } from "react";
+
 
 
 function Search() {
+    const[startDate, setStartDate] = useState(new Date());
+    const[endDate, setEndDate] = useState(new Date());
+    
+
+    const selectionRange = {
+          startDate: new Date(),
+          endDate: new Date(),
+          key: 'selection',  
+        }
+        endDate.setDate(endDate.getDate() + 1);
+
+        function handleSelect(ranges){
+            setStartDate(ranges.selection.startDate)
+            setEndDate(ranges.selection.endDate)
+        }  
+        
     return (
-        <div>
-        <h1>I am search</h1>
-            
+        <div className='search'>
+        <DateRangePicker
+        ranges={[selectionRange]}
+        onChange={handleSelect}/> 
         </div>
     )
 }
