0c62a00ef98c53a621a04185321d0d888c806dba
namacocnut <namacoconut@gmail.com>
2022-06-18 19:17:33 +0300

    fixing styling issues

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,37 +1,40 @@
-import React from 'react'
-import '../Header/Header.css'
-import HederIcon from '../../images/airbnb12.png'
-import SearchIcon from '@mui/icons-material/Search';
-import PublicIcon from '@mui/icons-material/Public';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Avatar from '@mui/material/Avatar';
-import {Link} from 'react-router-dom'
-
+import React from "react";
+import "../Header/Header.css";
+import HederIcon from "../../images/airbnb12.png";
+import SearchIcon from "@mui/icons-material/Search";
+import PublicIcon from "@mui/icons-material/Public";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Avatar from "@mui/material/Avatar";
+// import { Link } from "react-router-dom";
+import { Nav, NavLink, Bars, NavMenu } from "./NavbarElements";
 
 function Header() {
-    return (
-        <div className='header'>
-        <Link to='/'>
-        <img
-            className='header--icon'
-            src={HederIcon}
-            alt='airbnb-icon'
-        />
-        </Link>
-        <div className='header--center'>
-            <input type='text' />
-            <SearchIcon/>
-        </div>
-        <div className='header--right'>
-            <p>Become a host</p>
-            <PublicIcon/>
-            <ExpandMoreIcon/>
-            <Avatar/>
-
-        </div>
-            
-        </div>
-    )
+  return (
+    <Nav>
+      <NavLink to="/">
+        <img className="header--icon" src={HederIcon} alt="airbnb-icon" />
+      </NavLink>
+      <Bars />
+      <NavMenu>
+        <NavLink to="serch" activeStyle className="header--center">
+          <input type="text" />
+          <SearchIcon />
+        </NavLink>
+        <NavLink to="becomeHost" activeStyle>
+          <p>Become a host</p>
+        </NavLink>
+        <NavLink to="avatar" activeStyle>
+          <Avatar />
+        </NavLink>
+        <NavLink to="public" activeStyle>
+          <PublicIcon />
+        </NavLink>
+        <NavLink to="expand" activeStyle>
+          <ExpandMoreIcon />
+        </NavLink>
+      </NavMenu>
+    </Nav>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,47 +1,46 @@
-import React from 'react'
-import '../Search/Search.css'
-import {DateRangePicker} from 'react-date-range'
-import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
+import React from "react";
+import "../Search/Search.css";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
 import { useState } from "react";
-import EmojiPeopleOutlinedIcon from '@mui/icons-material/EmojiPeopleOutlined';
-import Button from '@mui/material/Button';
-import {useNavigate} from 'react-router-dom'
-
-
-
-
+import EmojiPeopleOutlinedIcon from "@mui/icons-material/EmojiPeopleOutlined";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 
 function Search() {
-    const navigate = useNavigate();
-    function navigateLink() {
-        navigate('/search')
-    }
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
+  const navigate = useNavigate();
+  function navigateLink() {
+    navigate("/search");
+  }
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
-    const selectionRange = {
-        startDate: startDate,
-        endDate: endDate,
-        key: "selection",
-      };
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
 
-    function handleSelect(ranges) {
-        setStartDate(ranges.selection.startDate);
-        setEndDate(ranges.selection.endDate);
-    }
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
 
-    return (
-        <div className='search'>
-            <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
-            <h2>
-                Number of guests <EmojiPeopleOutlinedIcon />
-            </h2>
-            <input min={0} defaultValue={2} type="number" />
-        <Button onClick={navigateLink}> Search Airbnb</Button>
-
-        </div>
-    )
+  return (
+    <div className="search">
+      <DateRangePicker
+        ranges={[selectionRange]}
+        onChange={handleSelect}
+        className="selection--range"
+      />
+      <h2 className="search--input--placeholder">
+        Number of guests <EmojiPeopleOutlinedIcon />
+      </h2>
+      <input className="search--input" min={0} defaultValue={2} type="number" />
+      <Button onClick={navigateLink}> Search Airbnb</Button>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
